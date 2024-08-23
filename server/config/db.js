d92d06aec2824d836b4f2050b6bebc12b0864877
Summer H <sh.hardison.sh@icloud.com>
2024-08-23 12:55:46 -0400

    Updates connections

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dealership');
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const connectDB = require('./config/connection');
+const connectDB = require('./config/db');
 const typeDefs = require('./schemas/typeDefs');
 const resolvers = require('./schemas/resolvers');
 const authMiddleware = require('./utils/auth');
