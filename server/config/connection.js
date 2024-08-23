b687821387baff3d8ae7b187bbe842725d66fb56
CameronGillum <GillumTCameron@gmail.com>
2024-08-23 11:56:03 -0400

    fixed the errors leading server not to start

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dealership');
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dealership');
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
 
-module.exports = mongoose.connection;
+module.exports = connectDB;
